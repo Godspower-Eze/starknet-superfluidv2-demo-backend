afec24a3c70bdd43bc62f43765be7fc8a720b915
Godspower Eze <Godspowereze260@gmail.com>
2023-06-09 17:21:01 +0100

    fix: corrected urls

--- superfluid/main/urls.py
+++ superfluid/main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('pools/', AllPools.as_view(), name="pools"),
     path('pools_by_account/<str:account>',
          PoolsByAccount.as_view(), name="pools_by_account"),
-    path('delete_pool/<str:address>/<str:account>/<str:token>/',
+    path('delete_pool/<str:address>/',
          DeletePool.as_view(), name="delete_pool"),
     path('create_pool/', CreatePool.as_view(), name="create_pool"),
 ]
